d5bb8c8f9c6858ee7d38aaf6c46fd63c77a71491
andresoranzzo <andresoranzzo@gmail.com>
2020-04-17 15:24:48 -0300

    Version 2: Added filter options.

--- app/src/main/java/com/example/capturepacket/CapturePacket.java
+++ app/src/main/java/com/example/capturepacket/CapturePacket.java
@@ -9,32 +9,65 @@ public class CapturePacket {
     public CapturePacket() {
         t = null;
     }
-
-    public void startTcpDump(){
-
-        try {
-            Runtime.getRuntime().exec(new String[]{"su", "-c", "chmod 777 /system/bin/tcpdump"});
-            t = new Thread(new Runnable() {
-                @Override
-                public void run() {
-                    try {
-                        Runtime.getRuntime().exec(new String[]{"su", "-c", "/system/bin/tcpdump -n -tt -i any >> /sdcard/tcpdump.log"});
-                    } catch (IOException e) {
-                        e.printStackTrace();
+    /*
+        public void startTcpDump(){
+            try {
+                Runtime.getRuntime().exec(new String[]{"su", "-c", "chmod 777 /system/bin/tcpdump"});
+                t = new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            Runtime.getRuntime().exec(new String[]{"su", "-c", "tcpdump -n -tt -i any >> /sdcard/tcpdump.log"});
+                            Runtime.getRuntime().exec(new String[]{"su", "-c", "tcpdump -i any -s 0 -w /sdcard/dump.pcap"});
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
                     }
+                });
+                t.start();
+                long start = System.currentTimeMillis();
+                long end = start + 5 * 1000; // 60 seconds * 1000 ms/sec
+                while (System.currentTimeMillis() < end) {
+                    t.sleep(500L);
                 }
-            });
-            t.start();
-            long start = System.currentTimeMillis();
-            long end = start + 10*1000; // 60 seconds * 1000 ms/sec
-            while (System.currentTimeMillis() < end)
-            {
-                t.sleep(500L);
-            }
-            t.interrupt();
+                t.interrupt();
 
-        } catch(IOException | InterruptedException e) {
-            e.printStackTrace();
+            } catch(IOException | InterruptedException e) {
+                e.printStackTrace();
+            }
         }
+        */
+    public void startTcpDump(final Boolean savePcapFormat, String hostFilter, String netFilter, String portFilter) throws IOException {
+        String filters = "";
+        if (!hostFilter.isEmpty()){
+            filters += "host " + hostFilter;
+        }
+        if(!netFilter.isEmpty()){
+            filters += "net " + netFilter;
+        }
+        if(!portFilter.isEmpty()){
+            filters += "port " + portFilter;
+        }
+        Runtime.getRuntime().exec(new String[]{"su", "-c", "chmod 777 /system/bin/tcpdump"});
+        final String finalFilters = filters;
+        t = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    if(!savePcapFormat)
+                        Runtime.getRuntime().exec(new String[]{"su", "-c", "tcpdump -n -tt -i any " + finalFilters + " >> /sdcard/tcpdump.log"});
+                    else
+                        Runtime.getRuntime().exec(new String[]{"su", "-c", "tcpdump -i any -s 0 -w " + finalFilters + " /sdcard/tcpdump.pcap"});
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        t.start();
     }
+
+    public void stopTcpDump() {
+        t.interrupt();
+    }
+
 }
--- app/src/main/java/com/example/capturepacket/MainActivity.java
+++ app/src/main/java/com/example/capturepacket/MainActivity.java
@@ -3,14 +3,87 @@ package com.example.capturepacket;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Switch;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    boolean isStart;
+    Button button;
+    TextView textView;
+    CapturePacket capture;
+    Switch logPcap;
+    Boolean savePcapFormat;
+    EditText ipNetFilter;
+    EditText ipHostFilter;
+    EditText portFilter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        CapturePacket capture = new CapturePacket();
-        capture.startTcpDump();
+        //Button start/stop capture
+        button = findViewById(R.id.button);
+        button.setOnClickListener(this);
+        textView = findViewById(R.id.textView);
+        reset();
+        //CapturePacket capture = new CapturePacket();
+       // capture.startTcpDump();
     }
+
+    private void reset() {
+        isStart = true;
+        textView.setText(R.string.inicial_message);
+        //Filters
+        ipHostFilter = findViewById(R.id.ip_host_address);
+        ipHostFilter.setText("");
+        ipNetFilter = findViewById(R.id.ip_net_address);
+        ipNetFilter.setText("");
+        portFilter = findViewById(R.id.port);
+        portFilter.setText("");
+        //File format (pcap ou log)
+        logPcap = (Switch) findViewById(R.id.switch_pcap);
+        logPcap.setChecked(false);
+    }
+
+    private void startButton(View view) throws IOException {
+        capture = new CapturePacket();
+        savePcapFormat = logPcap.isChecked();
+        capture.startTcpDump(savePcapFormat, ipHostFilter.getText().toString(), ipNetFilter.getText().toString(), portFilter.getText().toString());
+
+        textView.setText(R.string.start_message);
+    }
+
+    private void stopButton(View v) {
+        capture.stopTcpDump();
+        if(savePcapFormat)
+            textView.setText(R.string.stop_message_pcap);
+        else
+            textView.setText(R.string.stop_message_log);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(isStart){
+            try {
+                startButton(v);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            isStart = false;
+        } else {
+            stopButton(v);
+            isStart = true;
+        }
+
+
+            
+    }
+
+
 }
